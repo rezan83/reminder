a29ad36ed4ac8468c88af9dd57ad0433b33498c0
rezan83 <rezan.moh83@gmail.com>
2020-07-01 00:14:39 +0200

    Updates

---
+++ precache-manifest.7e784977ae10d71d8aa2d7a3cf2ab4da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99014bab73ed3484c850914512f8263e",
+    "url": "/reminder/index.html"
+  },
+  {
+    "revision": "b9d28a7c9c978029c67c",
+    "url": "/reminder/static/css/main.7e9749e2.chunk.css"
+  },
+  {
+    "revision": "a7ae3f1479276b8253f3",
+    "url": "/reminder/static/js/2.996cdd35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reminder/static/js/2.996cdd35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d28a7c9c978029c67c",
+    "url": "/reminder/static/js/main.29c59d9c.chunk.js"
+  },
+  {
+    "revision": "0e8934e113ca4337e377",
+    "url": "/reminder/static/js/runtime-main.1a1b7ec5.js"
+  }
+]);
