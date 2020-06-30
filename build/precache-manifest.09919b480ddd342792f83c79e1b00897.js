46db45f34a61999d85cc369cf3613d173566ae83
rezan83 <rezan.moh83@gmail.com>
2020-07-01 00:05:27 +0200

    add build2

---
+++ build/precache-manifest.09919b480ddd342792f83c79e1b00897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56a106b5c02f48ff842075336ca8d362",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9d28a7c9c978029c67c",
+    "url": "/static/css/main.7e9749e2.chunk.css"
+  },
+  {
+    "revision": "a7ae3f1479276b8253f3",
+    "url": "/static/js/2.996cdd35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.996cdd35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d28a7c9c978029c67c",
+    "url": "/static/js/main.29c59d9c.chunk.js"
+  },
+  {
+    "revision": "32902bda9dc28fdf4369",
+    "url": "/static/js/runtime-main.64e599a1.js"
+  }
+]);
